b29cb82c1210a6cc7eadde94922f636a75b3faf1
AlexKitTszLam <alexkittszlam@gmail.com>
2024-05-17 12:16:08 -0400

    Add files via upload

---
+++ StudyAssist/src/main/java/com/mycompany/studyassist/StudyAssist.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.studyassist;
+
+/**
+ *
+ * @author alex_
+ */
+public class StudyAssist {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

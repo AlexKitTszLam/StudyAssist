289307e907c3540fbed2e9276a39ae4d88f63a4e
AlexKitTszLam <alexkittszlam@gmail.com>
2024-05-21 12:14:04 -0400

    created a reader/setter for later

---
+++ StudyAssist/src/main/java/com/mycompany/studyassist/importExportMarks.java
@@ -0,0 +1,25 @@
+package com.mycompany.studyassist;
+
+import java.io.*;
+
+ public class importExportMarks {
+    BufferedWriter writer; //note that due to the filename being "marks.txt", it will overwrite the old file.
+     {
+         try {
+             writer = new BufferedWriter(new FileWriter("markFolder/marks.txt"));
+         } catch (IOException e) {
+             throw new RuntimeException(e);
+         }
+     }
+
+     BufferedReader reader;
+     {
+         try {
+             reader = new BufferedReader(new FileWriter("markFolder/marks.txt"));
+         } catch (IOException e) {
+             throw new RuntimeException(e);
+         }
+     }
+
+
+ }

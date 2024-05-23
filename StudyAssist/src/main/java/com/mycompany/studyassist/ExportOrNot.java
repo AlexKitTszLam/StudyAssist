733fd90f1fb5960b04abc9c46ddcc05ceb4c5a08
AlexKitTszLam <alexkittszlam@gmail.com>
2024-05-23 11:17:10 -0400

    comments
    Added descriptions of what methods in importexport and exportornot

--- StudyAssist/src/main/java/com/mycompany/studyassist/ExportOrNot.java
+++ StudyAssist/src/main/java/com/mycompany/studyassist/ExportOrNot.java
@@ -1,13 +1,15 @@
 package com.mycompany.studyassist;
 
-import javax.swing.JOptionPane;
+//This is a method for the gui that allows for the gui to ask for if you want to export the file when exiting.
+//It uses a YES/NO jOptionPane to ask for an answer.
 
+import javax.swing.JOptionPane;
 import static com.mycompany.studyassist.importExportMarks.writeIntoFile;
 
 public class ExportOrNot {
 
     public static void ExportQuestionPopup(){
-        int export = JOptionPane.showConfirmDialog(null, "Would you like to export your marks?", "FORTNITE BATTLE PASSS", JOptionPane.YES_NO_OPTION);
+        int export = JOptionPane.showConfirmDialog(null, "Would you like to export your marks?", "export????????????????????????", JOptionPane.YES_NO_OPTION);
         if(export == JOptionPane.YES_OPTION){
             writeIntoFile();
             System.out.println("wrote to file");
--- StudyAssist/src/main/java/com/mycompany/studyassist/importExportMarks.java
+++ StudyAssist/src/main/java/com/mycompany/studyassist/importExportMarks.java
@@ -3,7 +3,8 @@ package com.mycompany.studyassist;
 import java.io.*;
 
  public class importExportMarks {
-    static BufferedWriter writer; //note that due to the filename being "marks.txt", it will overwrite the old file.
+
+    static BufferedWriter writer; //note that due to the filename being "marks.txt", it will overwrite the old file. This is by design, as it should be writing to one file to allow for the person to actually view marks after rebooting.
      {
          try {
              writer = new BufferedWriter(new FileWriter("markFolder/marks.txt"));
@@ -12,6 +13,7 @@ import java.io.*;
          }
      }
 
+     //it's a reader.
      static BufferedReader reader;
      {
          try {
@@ -21,6 +23,7 @@ import java.io.*;
          }
      }
 
+     //This method is so the array is made into a format that is easier for the writer to write into.
      public static void writeIntoFile(){
          StringBuilder whatToPrint = new StringBuilder();
          for(Gui.Mark mark: Gui.markArr){
